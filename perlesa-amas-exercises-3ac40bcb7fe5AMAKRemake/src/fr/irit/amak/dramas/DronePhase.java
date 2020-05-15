b123fbfac844fa58b0342b447fd10cfebd674df1
RemakeTm <lemeunier.axel@gmail.com>
2020-05-15 16:10:24 +0200

    DronePhase

---
+++ perlesa-amas-exercises-3ac40bcb7fe5AMAKRemake/src/fr/irit/amak/dramas/DronePhase.java
@@ -0,0 +1,13 @@
+package fr.irit.amak.dramas;
+
+import fr.irit.smac.amak.AgentPhase;
+
+public class DronePhase extends AgentPhase<DrAmas, World> {
+	
+	public DronePhase (Drone drone) {
+		setAgent(drone);
+	}
+	
+	
+
+}
